c2f4db31296d1971e3305fe595534ec374bda0f0
bekamine <aminebekri@gmail.com>
2025-12-07 03:14:59 +0100

    Config: Update for deployment to aminebekri.fr

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/demo-avocat/', // Le nom de votre repo entre slashs
+  base: '/',  // <--- CHANGEZ CECI (Mettez juste un slash)
   plugins: [react()],
 })
-
